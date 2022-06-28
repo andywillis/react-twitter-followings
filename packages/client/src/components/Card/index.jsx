a2993139fe3654f9a2f50801bcf0802bfac8c6d1
Andy Willis <andywillis@users.noreply.github.com>
2022-06-28 09:32:32 +0100

    Placeholders (#1)
    Co-authored-by: Andy Willis <awillis@fastmail.fm>

--- packages/client/src/components/Card/index.jsx
+++ packages/client/src/components/Card/index.jsx
@@ -1,23 +1,46 @@
+import { useState } from 'react';
+import classnames from 'classnames';
+
 import Image from '../Image';
 
 import styles from './index.module.css';
 
 function Card({ card }) {
 
+  const [ isLoaded, setIsLoaded ] = useState(false);
+
   const { name, img, href } = card;
 
+  function handleImageLoad() {
+    setIsLoaded(true);
+  }
+
+  const cardStyle = classnames({
+    [styles.card]: true,
+    [styles.hidden]: !isLoaded
+  });
+
+  const anchorStyle = classnames({
+    [styles.anchor]: true,
+    [styles.disabled]: !isLoaded
+  });
+
   return (
-    <a href={href}>
-      <div className={styles.card}>
-        <div className={styles.details}>
-          <Image src={img} alt={name} />
-          <div className={styles.name}>
-            {name}
+    <a href={href} className={anchorStyle}>
+      <div className={styles.container}>
+        <div className={cardStyle}>
+          <div className={styles.details}>
+            <Image src={img} alt={name} handleImageLoad={handleImageLoad} />
+            <div className={styles.name}>
+              {name}
+            </div>
           </div>
         </div>
+        {!isLoaded && <div>&nbsp;</div>}
       </div>
     </a>
   );
+
 }
 
 export default Card;
