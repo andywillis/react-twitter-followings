b3d9824c638b3834e7384eff495be8e827cfb1ad
Andy Willis <awillis@fastmail.fm>
2022-06-28 09:31:16 +0100

    Placeholders

--- packages/client/src/components/Image/index.jsx
+++ packages/client/src/components/Image/index.jsx
@@ -2,14 +2,19 @@ import dummyImg from './dummy.png';
 
 import styles from './index.module.css';
 
-function Image({ alt, src }) {
+function Image({ alt, src, handleImageLoad }) {
+
+  function handleError(e) {
+    e.target.src = dummyImg;
+  }
 
   return (
     <div className={styles.img}>
       <img
         alt={alt}
         src={src}
-        onError={function (e) { e.target.src = dummyImg; }}
+        onLoad={handleImageLoad}
+        onError={handleError}
       />
     </div>
   );
