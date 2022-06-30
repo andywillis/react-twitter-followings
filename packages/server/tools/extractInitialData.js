2ea319c6e2de7bd9767c9dc20c84376ad3eca6d5
Andy Willis <awillis@fastmail.fm>
2022-06-30 14:56:23 +0100

    Updated convertXML

--- packages/server/tools/extractInitialData.js
+++ packages/server/tools/extractInitialData.js
@@ -1,3 +1,17 @@
+import couch from '../service/couch';
+
 import zoe from '../data/json/zcumming.json' assert { type: 'json' };
 
-console.log(zoe);
+const {
+  rss: {
+    channel: {
+      name,
+      item
+    }
+  }
+} = zoe;
+
+console.log(name, item);
+
+const dates = item.map(obj => obj.pubDate);
+console.log(dates);
