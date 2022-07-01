1b41cad81ff1f7359d38a1421f470e3057ecc84b
Andy Willis <awillis@fastmail.fm>
2022-07-01 11:13:08 +0100

    Added sse back

--- packages/server/index.js
+++ packages/server/index.js
@@ -4,8 +4,11 @@ import dirname from './helpers/dirname';
 
 import routes from './routes/index'
 
+import sseFunction from './helpers/sseFunction';
+
 import followings from './data/followings.json' assert { type: 'json' };
 
+app.get('/stream', routes.sse(sseFunction));
 app.get('/followings', routes.followings(followings));
 app.get('/', routes.root());
 
